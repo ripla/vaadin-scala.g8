d8d746dc697b0369d3be1f6f9f7f66f0e8d331fa
Risto Yrjänä <ristoy@me.com>
2011-12-16 21:42:45 +0200

    first version of working project files

---
+++ src/main/g8/src/main/scala/$package$/$classname$Application.scala
@@ -0,0 +1,10 @@
+import com.vaadin.Application
+import vaadin.scala._
+
+
+class $classname$Application extends Application {
+	def init(): Unit = {
+		setMainWindow(new Window("$name$"))
+		getMainWindow.addComponent(new Label("This Vaadin app uses ScalaWrappers!"))
+	}
+}
