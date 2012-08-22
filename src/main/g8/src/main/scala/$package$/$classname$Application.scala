33c10e35df462e806a4d7664405fec6471e9142a
Risto Yrjänä <risto.yrjana@vaadin.com>
2012-08-22 11:50:30 -0400

    Add margins to template layout

--- src/main/g8/src/main/scala/$package$/$classname$Application.scala
+++ src/main/g8/src/main/scala/$package$/$classname$Application.scala
@@ -4,6 +4,7 @@ import vaadin.scala._
 
 class $classname$Application extends Application("$name$") {
 	override val main: ComponentContainer = new VerticalLayout {
+		margin = true
 		components += Label("This Vaadin app uses Scaladin!")
 	}
 }
